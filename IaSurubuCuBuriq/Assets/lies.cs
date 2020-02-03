db8ee21add47636286f04c21adb2ee654cbaf57b
geococcyx <geococcyx@github.com>
2020-02-03 21:44:48 +0200

    un rahat.

---
+++ IaSurubuCuBuriq/Assets/lies.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class lies : MonoBehaviour
+{
+    public int height;
+    
+    // Update is called once per frame
+    void Update(){
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+            GetComponent<RectTransform>();
+            height += 100;
+            }
+        }
+}
