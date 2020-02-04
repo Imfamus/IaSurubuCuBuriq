63d80d552b1ff31aba41264167c89d8868629ab3
geococcyx <geococcyx@github.com>
2020-02-04 08:29:24 +0200

    rahat 2.o

---
+++ IaSurubuCuBuriq/Assets/uicontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uicontroller : MonoBehaviour
+{
+    //private int V = 90;
+    public int value = 0;
+    public GameObject bara;
+    public Vector3 position;
+
+    private void OnMouseDown()
+    {
+       // GetComponent<RectTransform>().rotation.x = V;
+    }
+
+
+
+    void Update()
+    {
+        // update money
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("moneyz added");
+            value += 10;
+        }
+    }
+}
